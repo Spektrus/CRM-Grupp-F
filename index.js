51c7b006658274c03ad446668e9bbc513958f012
kefri1901 <kevin.fridman@yh.nackademin.se>
2019-10-26 14:00:15 +0200

    added login function

---
+++ index.js
@@ -0,0 +1,31 @@
+    var loginUser = [
+    {
+        username: "ashur",
+        password: "admin"
+    },
+    {
+        username: "kevin",
+        password: "admin"
+    },
+    {
+        username: "mathias",
+        password: "admin"
+    }
+]
+
+
+    function logIn(){
+    let username = document.getElementById("loginuser").value
+    let password = document.getElementById("loginpw").value
+
+    for(i = 0; i < loginUser.length; i++){
+
+        if(username == loginUser[i].username && password == loginUser[i].password){
+            window.location.href =  window.location+ "/crm/index.html";
+            console.log( username + " You are logged in!");
+            return
+            
+        }
+    } 
+    alert("Wrong username or password");
+}
