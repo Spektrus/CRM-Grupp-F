99480776c7207b1ef8b027e72a931612de4eb8bc
Mathias Beckius <lillmatte@hotmail.com>
2019-10-26 16:30:43 +0200

    login cookie

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
-    var loginUser = [
-    {
+var loginUser = [{
         username: "ashur",
         password: "admin"
     },
@@ -13,20 +12,22 @@
     }
 ]
 
-
-    function logIn(){
+function logIn() {
     let username = document.getElementById("loginuser").value
     let password = document.getElementById("loginpw").value
 
-    for(i = 0; i < loginUser.length; i++){
 
-        if(username == loginUser[i].username && password == loginUser[i].password){
+    for (i = 0; i < loginUser.length; i++) {
+
+        if (username == loginUser[i].username && password == loginUser[i].password) {
             window.location.href = "crm/index.html";
-            console.log( username + " You are logged in!");
-            return
-            
+            console.log(username + " You are logged in!");
+            let date = new Date();
+            date.setDate(date.getDate()+1);
+            document.cookie = "login=true; expires=" + date + "; path=/;";
+            return;
         }
-    } 
+    }
     alert("Wrong username or password");
-    
+
 }
--- script.js
+++ script.js
@@ -171,5 +171,8 @@ $(function () {
 });
 
 function logOut(){
-    window.location.href = "/CRM-Grupp-F/index.html";
+    document.cookie = "login=false; expires=Thu, 01 Jan 1970 00:00:01 UTC; path=/;"
+    if (document.cookie !== "login=true") {
+        window.location.href = "../";
+    }
 }
