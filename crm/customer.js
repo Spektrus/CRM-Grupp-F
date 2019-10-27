f79f003b21a7c97c7c47b681fb9da3e85b37befb
ashurw <ashurw@gmail.com>
2019-10-27 15:39:44 +0100

    I have added so that you can remove and edit customer, but when editing the table is not updated and thers is some small problem with the additional information field

--- crm/customer.js
+++ crm/customer.js
@@ -1,59 +1,118 @@
-const addButton = document.getElementById("addContactButton");
-const firstNameSelect = document.getElementById("customerFirstNameInput");
-const lastNameSelect = document.getElementById("customerLastNameInput");
-const phoneSelect = document.getElementById("customerPhoneInput");
-const emailSelect = document.getElementById("customerEmailInput");
-const additionalSelect = document.getElementById("customerAdditionalInput");
-const tbodySelect = document.getElementById("customerTbody");
+const addButton = $("#addContactButton");
+const customerNameSelect = $("#customerNameInput");
+const phoneSelect = $("#customerPhoneInput");
+const emailSelect = $("#customerEmailInput");
+const additionalSelect = $("#customerAdditionalInput");
+const tbodySelect = $("#customerTbody");
 
 let customerTableArray = [];
+let customerName;
+let phone;
+let email;
+let additional;
+let info;
 
-addButton.addEventListener("click", e => {
+
+addButton.on("click", function() {
     console.log("New Customer added");
-    let firstName = firstNameSelect.value;
-    let lastName = lastNameSelect.value;
-    let phone = phoneSelect.value;
-    let email = emailSelect.value;
-    let additional = additionalSelect.value;
-    console.log(firstName, lastName, phone, email, additional);
+    customerName = customerNameSelect.val();
+    phone = phoneSelect.val();
+    email = emailSelect.val();
+    additional = additionalSelect.val();
+    console.log(customerName, phone, email, additional);
+    
+    addCustomerToTableArray(customerName, phone, email, additional);
 
-
-    addCustomerToTableArray(firstName, lastName, phone, email, additional);
-
-    printCustomerToTable(firstName, lastName, phone, email);
+    printCustomerToTable(customerName, phone, email);
 
     console.log(customerTableArray);
-    addButton.setAttribute("data-dismiss", "modal");
-    firstNameSelect.value = "";
-    lastNameSelect.value = "";
-    phoneSelect.value = "";
-    emailSelect.value = "";
-    additionalSelect.value = "";
+    addButton.attr("data-dismiss", "modal");
+    customerNameSelect.val("");
+    phoneSelect.val("");
+    emailSelect.val("");
+    additionalSelect.val("");
 });
 
 
 
 
-function addCustomerToTableArray(first, last, phone, email, additional){
-    objC = {
-        name: (first + " " + last),
+function addCustomerToTableArray(fullname, phone, email, additional){
+    let objC = {
+        name: fullname,
         phoneNr: phone,
         mail: email,
         information: additional
-    }
+    };
 
     customerTableArray.push(objC);
 }
 
-function printCustomerToTable(first, last, phone, email) {
-    let trow = document.createElement("tr");
-    let tRowNumber = document.createElement("th");
-    let n = document.createElement("td");
-    let p = document.createElement("td");
-    let e = document.createElement("td");
-    n.innerText = first + " " + last
-    p.innerText = phone;
-    e.innerText = email;
-    trow.append(tRowNumber, n, p, e);
-    tbodySelect.append(trow)
+function printCustomerToTable(name, phone, email) {
+    $("#customerTbody").append("<tr id='cID" + (customerTableArray.length) + "'>" + "<th scope='row'>" + (customerTableArray.length) + "</th>" + "<td class='cName'>" + name +"</td>" + "<td class='cPhone'>" + phone +"</td>" + "<td class='cMail'>" + email +"</td>" + "</tr>");
+    
+    // let trow = document.createElement("tr");
+    // let tRowNumber = document.createElement("th");
+    // let n = document.createElement("td");
+    // n.classList.add("fullname");
+    // let p = document.createElement("td");
+    // let e = document.createElement("td");
+    // let more = document.createElement("td")
+    // tRowNumber = customerTableArray.length;
+    // n.innerText = first + " " + last
+    // p.innerText = phone;
+    // e.innerText = email;
+    // trow.append(tRowNumber, n, p, e);
+    // tbodySelect.append(trow)
 }
+
+
+//Click/selected customer
+$("#customerTbody").on("click", "tr", function() {
+    $(this).attr("data-toggle", "modal");
+    $(this).attr("data-target", "#editNewContactWindow");
+    $(this).attr("data-picked", "true");
+    $("#customerNameVal").val($(this).children(".cName").text());
+    $("#customerPhoneVal").val($(this).children(".cPhone").text());
+    $("#customerEmailVal").val($(this).children(".cMail").text());
+    info = $.grep(customerTableArray, function(e) {
+        return e.name == $("#customerNameVal").val();
+    });
+    console.log(info);
+    $("#customerAdditionalVal").val(info[0].information);
+
+    // Remove selected contact
+    $("#removeContactButton").on("click", function() {
+        for(var i = 0; i < customerTableArray.length; i++) {
+            if(customerTableArray[i].information == info.information) {
+                customerTableArray.splice(i, 1);
+                break;
+            }
+        }
+       info = "";
+        console.log(customerTableArray);
+        $(this).attr("data-dismiss", "modal");
+        $("tr[data-picked='true']").remove();
+
+    });
+    $("#editContactButton").on("click", function() {
+        for(var i = 0; i < customerTableArray.length; i++) {
+            if(customerTableArray[i].name == info.name) {
+                let tmp = {
+                    name: $("#customerNameVal").val(),
+                    phoneNr: $("#customerPhoneVal").val(),
+                    mail: $("#customerEmailVal").val(),
+                    information: $("#customerAdditionalVal").val()
+                };
+                customerTableArray.splice(i, 1);
+                customerTableArray.push(tmp);
+                break;
+            }
+        }
+        info = "";
+        console.log(customerTableArray);
+        $(this).attr("data-dismiss", "modal");
+        $("tr[data-picked='true']").attr("data-picked", "false")
+    });
+})
+
+
