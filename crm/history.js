bef55221e347576950c8979b3141453632281e04
kefri1901 <kevin.fridman@yh.nackademin.se>
2019-11-01 11:35:47 +0100

    added function to inject api people to historylist

--- crm/history.js
+++ crm/history.js
@@ -1,15 +1,9 @@
-let costumer1;
-let costumer2;
-let costumer3;
-let costumer4;
-let costumer5;
+let historyName;
+let historyCompany;
+let historyDate;
+let historyEvent;
+let historyPeople= []
 
-const addButton = $("#addContactButton");
-const customerNameSelect = $("#customerNameInput");
-const phoneSelect = $("#customerPhoneInput");
-const emailSelect = $("#customerEmailInput");
-const additionalSelect = $("#customerAdditionalInput");
-const tbodySelect = $("#customerTbody");
 
 
 
@@ -19,28 +13,41 @@ $.ajax({
     url: "https://5db6da6bf6869d001474a972.mockapi.io/api/crm/costumer"
   })
     .done(function( data ) {
+     
+      for(let i = 0; i < data.length; i++){
+        historyName = data[i].name;
+        historyCompany = data[i].Company;
+         historyDate = data[i].date;
+         historyEvent = data[i].Event;
+
+
         
-      customer1 = data[0]
-      customer2 = data[1]
-      customer3 = data[2]
-      customer4 = data[3]
-      customer5 = data[4]
-      console.log(data);
+         historyPeople.push(historyName, historyCompany, historyDate, historyEvent);
+         
+       
+      }
+      //historyPeople = data;
 
-
+    
       
-      //CustomerName
-      document.getElementById("name_1").innerHTML = customer1.name;
-      document.getElementById("name_2").innerHTML = customer2.name;
-      document.getElementById("name_3").innerHTML = customer3.name;
-      document.getElementById("name_4").innerHTML = customer4.name;
-      document.getElementById("name_5").innerHTML = customer5.name;
-      //Costumercompany
-      document.getElementById("company_1").innerHTML = customer1.Company;
-      document.getElementById("company_2").innerHTML = customer2.Company;
-      document.getElementById("company_3").innerHTML = customer3.Company;
-      document.getElementById("company_4").innerHTML = customer4.Company;
-      document.getElementById("company_5").innerHTML = customer5.Company;
-
-
+      printHistory();
+    
     });
+
+   
+
+    console.log(historyPeople);
+    
+ 
+    function printHistory() {
+      
+      $("#historyTbody").empty();
+      for (let i = 0; i < historyPeople.length; i++) {
+          $("#historyTbody").append("<tr id='cID" + i + "' data-toggle='tooltip' data-placement='bottom'" + "<th scope='row'>" + historyPeople[i] + "</th>" + "<td>" + historyPeople[i].historyName + "</td>" + "<td  d-none d-lg-table-cell'>" + historyPeople[i].historyCompany + "</td>" + "<td d-none d-sm-table-cell'>" + historyPeople[i].historyEvent + "</td>" +  + "<td>" + historyPeople[i].historyEvent+ "</td>" +  + "<td>" + historyPeople[i].historyDate+ "</td>" + "</tr>");
+      }
+      $('[data-toggle="tooltip"]').tooltip({
+          trigger: 'hover'
+      });
+  }
+    
+
